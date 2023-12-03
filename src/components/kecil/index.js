6d05f89030342b6855d05303fe2a719a744744d7
Ansar Nur Jamas <jamasguitarist@gmail.com>
2023-12-03 22:08:11 +0700

    NoteApp

---
+++ src/components/kecil/index.js
@@ -0,0 +1,7 @@
+import BackFAB from './back_fab'
+import Button from './button' 
+import Input from './input'
+import CategoryTab from './categoryTab'
+import Pilihan from './pilihan'
+
+export {BackFAB, Button, Input, CategoryTab, Pilihan}
